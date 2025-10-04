01e70eed915d6a3b71718cbfdcb049d3c197dcb6
FacuAlonso <112171600+FacuAlonso@users.noreply.github.com>
2025-10-04 00:42:14 -0300

    Commit inicial

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+const PORT = 3000;
+
+export const API_CONFIG = {
+  PORT,
+  BASE_URL: `http://localhost:${PORT}`,
+};
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import { API_CONFIG } from "./config";
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await prisma.product.findMany();
+    res.json(products);
+  } catch(error){
+    res.status(500).json({error: "Error al consultar DB de productos"})
+  }
+})
+
+app.post("/products", async (req, res) => {
+  const {name, price, description, image} = req.body;
+  try {
+    const new_product = await prisma.product.create({
+      data: {name, price, description, image}
+    });
+    res.json(new_product);
+  } catch(error){
+    res.status(400).json({error : "Error al crear el producto"})
+  }
+})
+
+app.listen(API_CONFIG.PORT, () => {
+  console.log(`🚀 Servidor corriendo en ${API_CONFIG.BASE_URL}`);
+});
